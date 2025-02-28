818db19a875e3825d41f010b6e39dca7718885e4
Vital Rusinov <Vitaliy.rusinov90@gmail.com>
2025-02-28 15:07:07 +0500

    task completed

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Функция для проверки, находится ли элемент в поле зрения
+// <--------------Плавное появление элементов (начало)---------------->
 function isElementInViewport(el) {
   const rect = el.getBoundingClientRect();
   return rect.top < window.innerHeight && rect.bottom > 0;
 }
 
-// Функция для обработки всех элементов
 function handleVisibility() {
   document.querySelectorAll('body *').forEach((element) => {
       if (isElementInViewport(element)) {
@@ -13,6 +12,35 @@ function handleVisibility() {
   });
 }
 
-// Выполняем проверку сразу при загрузке и при прокрутке
 document.addEventListener("DOMContentLoaded", handleVisibility);
 window.addEventListener("scroll", handleVisibility);
+// <--------------Плавное появление элементов (конец)---------------->
+
+// <--------------Событие для кнопки сабмит (начало)---------------->
+const form = document.getElementById('order-form_form');
+const submitBtn = document.getElementById('submitBtn');
+const agreement = document.getElementById('agreement');
+
+agreement.addEventListener('change', () => {
+  submitBtn.disabled = !agreement.checked;
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  formData.forEach((value, key) => {
+      console.log(`${key}: ${value}`);
+  });
+});
+// <--------------Событие для кнопки сабмит (конец)---------------->
+
+// <--------------Событие для кнопок Order (начало)---------------->
+document.querySelectorAll(".toOrder").forEach(button => {
+  button.addEventListener("click", function () {
+      document.querySelector("#order").scrollIntoView();
+  });
+});
+
+// <--------------Событие для кнопок Order (конец)---------------->
